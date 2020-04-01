dec2ee1c04af337b58c63a598035c329eb48b393
enocpi <61128286+enocpi@users.noreply.github.com>
2020-03-31 20:55:53 -0500

    Add files via upload

---
+++ lab10_4mediap/lab10_4mediap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab10_4mediap
+{
+    interface IAutoPlayer
+    {
+        void play();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            artist prince = new artist("prince");
+            prince.AddToPlaylist("purp rain");
+            prince.AddToPlaylist("LRC");
+            prince.AddToPlaylist("KISS");
+            prince.AddToPlaylist("doves cry");
+
+            media player = new media();
+            player.playermedia(prince);
+
+            audiob book = new audiob ("how to win friends","dale");
+            player.playermedia(book);
+
+            moviest starw = new moviest("starwars");
+            starw.SelectTrack("theme");
+            player.playermedia(starw);
+
+
+        }
+    }
+}
---
+++ lab10_4mediap/lab10_4mediap/audiob.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10_4mediap
+{
+    class audiob
+    {
+    }
+}
